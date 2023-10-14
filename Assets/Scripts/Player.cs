a1be86e77c7c555c21392be42a31c088ad8939e5
Windfinz <Phongdraco@gmail.com>
2023-10-14 17:09:41 +0700

    Death anim

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animation smallRenderer;
+    public Animation bigRenderer;
+
+    private DeathAnim deathAnim;
+    
+    public bool big => bigRenderer.enabled;
+    public bool small => smallRenderer.enabled;
+    public bool death => deathAnim.enabled;
+
+
+    private void Awake()
+    {
+        deathAnim = GetComponent<DeathAnim>();
+    }
+    public void Hit()
+    {
+        if (big)
+        {
+            Shrink();
+        }else
+        {
+            Death();
+        }
+    }
+
+    private void Shrink()
+    {
+
+    }
+    private void Death()
+    {
+        smallRenderer.enabled = false;
+        bigRenderer.enabled = false;
+        deathAnim.enabled = true;
+
+        GameManager.Instance.ResetLevel(3f);
+    }
+
+}
